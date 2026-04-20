90ce8da12fb403a46c14b26e09b82d49dc3e7500
firasj082 <firasj082@gmail.com>
2026-04-20 11:01:59 +0300

    Moved the exceptions folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import exceptions.InvalidDataException;
+import com.mycompany.app.exceptions.InvalidDataException;
 
 public class App {
 
